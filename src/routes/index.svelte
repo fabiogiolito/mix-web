14d5205ac985070a6e03073fb77e681ad70454d2
Fabio Giolito <fabiogiolito@gmail.com>
2022-05-27 19:48:18 +0100

    update qr code, update logic for curators (no update)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,7 +2,9 @@
   import { onMount, onDestroy } from "svelte";
   import { fly } from "svelte/transition"
 
+  const DOWNLOAD_URL = "https://link.mix.com/byTbvDe29E";
   const IOS_DOWNLOAD_URL = "https://apps.apple.com/pt/app/mix-rediscover-the-internet/id1092817691";
+  const ANDROID_DOWNLOAD_URL = "https://play.google.com/store/apps/details?id=com.mix.android";
   const TWITTER_URL = "https://twitter.com/getmixapp";
   const INSTAGRAM_URL = "https://www.instagram.com/getmixapp/";
 
@@ -16,33 +18,34 @@
   $: printContent = contentList[currentContentIndex];
 
   // Randomly update curator count
-  let curatorsUpdateTimeout; // Reference to clear timeout on destroy
+  // let curatorsUpdateTimeout; // Reference to clear timeout on destroy
   let curatorsCount = getCuratorsCount(); // Number of curators on platform
   $: printCuratorsCount = curatorsCount.toLocaleString("en-US"); // Printable version of count with commas (reactive)
 
   // Get a fake number of curators
   function getCuratorsCount() {
-    // Freeze curators count for now
-    return 15743;
-    // return Math.floor(new Date().getTime() / 1000) - 1653579588
+    const DEFAULT_CURATOR_COUNT = 12691;
+    const DEFAULT_CURATOR_COUNT_TIMESTAMP = 1651092248269;
+    const daysSinceStamp = Math.floor((Date.now() - new Date(DEFAULT_CURATOR_COUNT_TIMESTAMP)) / (1000 * 3600 * 24));
+    return DEFAULT_CURATOR_COUNT + (109 * daysSinceStamp);
   }
 
   // Recursive timeout creation with random duration
-  function createCuratorsTimeout(callback) {
-    // Get random seconds between 3 and 10
-    let seconds = Math.floor(Math.random() * (20 - 7 + 1)) + 7
+  // function createCuratorsTimeout(callback) {
+  //   // Get random seconds between 3 and 10
+  //   let seconds = Math.floor(Math.random() * (20 - 7 + 1)) + 7
 
-    // Create timeout
-    curatorsUpdateTimeout = setTimeout(() => {
+  //   // Create timeout
+  //   curatorsUpdateTimeout = setTimeout(() => {
 
-      // Create new timeout
-      createCuratorsTimeout(callback);
+  //     // Create new timeout
+  //     createCuratorsTimeout(callback);
 
-      // Execute callback
-      callback();
+  //     // Execute callback
+  //     callback();
 
-    }, seconds * 1000);
-  }
+  //   }, seconds * 1000);
+  // }
 
   function handleToggleQRCode() {
     QRCodeVisible = !QRCodeVisible;
@@ -55,16 +58,14 @@
     }, 2000);
 
     // Initiate curators count update
-    createCuratorsTimeout(() => {
-      curatorsCount = getCuratorsCount();
-    });
-
-    
+    // createCuratorsTimeout(() => {
+    //   curatorsCount = getCuratorsCount();
+    // });
   });
 
   onDestroy(() => {
     clearInterval(contentTypeInterval);
-    clearTimeout(curatorsUpdateTimeout);
+    // clearTimeout(curatorsUpdateTimeout);
   });
 
 </script>
@@ -113,7 +114,7 @@
 
             <!-- Install button -->
             <p class="mb-8">
-              <button on:click={handleToggleQRCode} href={IOS_DOWNLOAD_URL} class="btn btn-primary btn-dot">
+              <button on:click={handleToggleQRCode} class="btn btn-primary btn-dot">
                 Get the Mix App
               </button>
             </p>
@@ -133,13 +134,16 @@
         <!-- Qr code visible -->
         {:else}
           <div in:fly={{ y: 40, duration: 200 }} class="relative z-10 -mt-24">
-            <a href={IOS_DOWNLOAD_URL} target="_blank" class="inline-block bg-white rounded-xl p-4 pb-5 md:p-6 mb-4 text-black text-center">
-              <img class="mx-auto w-56 h-56 mb-2" src="/qr_ios.png" alt="Download app" />
+            <a href={DOWNLOAD_URL} target="_blank" class="inline-block bg-white rounded-xl p-4 pb-5 md:p-6 mb-4 text-black text-center">
+              <img class="mx-auto w-56 h-56 mb-2" src="/qr_download.png" alt="Download app" />
               <span class="block font-medium text-lg">Scan to install</span>
             </a>
             <div class="space-y-2">
-              <a href={IOS_DOWNLOAD_URL} target="_blank" class="btn btn-secondary block w-full" on:click={handleToggleQRCode}>
-                iOS App Store
+              <a href={IOS_DOWNLOAD_URL} target="_blank" class="btn btn-secondary block w-full">
+                Install on iOS
+              </a>
+              <a href={ANDROID_DOWNLOAD_URL} target="_blank" class="btn btn-secondary block w-full">
+                Install on Android
               </a>
               <button class="btn block w-full" on:click={handleToggleQRCode}>Done</button>
             </div>

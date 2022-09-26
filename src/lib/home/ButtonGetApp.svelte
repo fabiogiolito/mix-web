975872287ee15bd5721ea71be69941b4fc740d12
fabiogiolito <fabiogiolito@gmail.com>
2022-09-26 19:17:46 +0100

    get the app with QR code

--- src/lib/home/ButtonGetApp.svelte
+++ src/lib/home/ButtonGetApp.svelte
@@ -19,17 +19,19 @@
 
   <!-- Mobile smart link -->
   {#if platform == "iOS" || platform == "Android"}
-    <a href={APP_DOWNLOAD_URL} class="btn btn-primary btn-dot">
-      Get the Mix App
+    <a href={APP_DOWNLOAD_URL} class="p-1 rounded bg-white w-20 mx-auto">
+      <img class="mx-auto w-full aspect-square" src="/qr_download.png" alt="Download app" />
+      <span class="block text-center text-xs text-black font-medium leading-none my-1">Get the app</span>
     </a>
+    <!-- <a href={APP_DOWNLOAD_URL} class="btn btn-primary btn-dot">
+      Get the Mix App
+    </a> -->
 
   <!-- Desktop or Undefined -->
   {:else}
-    <button
-      on:click={e => dispatch('showQRCode')}
-      class="btn btn-primary btn-dot"
-    >
-      Get the Mix App
+    <button on:click={e => dispatch('showQRCode')} class="p-1 rounded bg-white w-20 mx-auto">
+      <img class="mx-auto w-full aspect-square" src="/qr_download.png" alt="Download app" />
+      <span class="block text-center text-xs text-black font-medium leading-none my-1">Get the app</span>
     </button>
 
   {/if}
